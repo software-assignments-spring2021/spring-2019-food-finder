d3958054367fc41471f64f5a2fd0ae9d61b463eb
Ashwin <aktanuku96@gmail.com>
2019-03-05 01:27:35 -0500

    added validation file

---
+++ Test/test/ashwinValidateTest.js
@@ -0,0 +1,30 @@
+var ashwinValidateText = require('../ashwinTestFunction');
+var expect = require('chai').expect;
+
+describe('#ashwinTestFunction()', function(){
+
+	context('argument contains letters other then C', function() {
+		it('should throw error', function() {
+			expect(function() {
+				ashwinValidateText('Arabian')
+			}).to.throw(TypeError, 'ashwinTestFunction() expects only the letters C or c.')
+		})
+	})
+
+	context('argument contains numbers', function() {
+		it('should throw error', function() { 
+			expect(function() {
+				ashwinValidateText('214509105')
+			}).to.throw(TypeError, 'ashwinTestFunction() expects only the letters C or c.')
+		})
+	})
+
+	context('argument contains special characters,' function() {
+		it('should throw error', function() { 
+			expect(function() {
+				ashwinValidateText('*&^%$#$%^')
+			}).to.throw(TypeError, 'ashwinTestFunction() expects only the letters C or c.')
+		})
+	})
+
+})
