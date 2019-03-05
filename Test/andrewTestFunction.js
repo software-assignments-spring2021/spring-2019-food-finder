767a60d46a296bde32176a4458ac06bd41b280a5
Andrew Zaw <44329973+azaw502@users.noreply.github.com>
2019-03-05 15:40:38 -0500

    Fixing text

--- Test/andrewTestFunction.js
+++ Test/andrewTestFunction.js
@@ -4,7 +4,7 @@ module.exports = function()
 { 
   var args = Array.prototype.slice.call(arguments);
   
-  // Throw error if arguments is below 0
+  // Throw error if number entered Fis below 0
   if (args < 0)
     {
       throw new TypeError('andrewTestFunction() expects a positive number')
