16b138bd6cc1e60b656cf5827bfb3fd18244d59d
ak5352 <ak5352@nyu.edu>
2019-05-02 12:33:40 -0400

    Removing blank restaurants and unicode characters

--- Data/Scrubbing Process/remove_dups_function.py
+++ Data/Scrubbing Process/remove_dups_function.py
@@ -3,14 +3,17 @@ def remove_duplicates(f, target):
     print("Working on your file")
     t = open(target, 'w')
 
-    seen = set()                                            # set will keep track of duplicates
+    seen = set()                                                # set will keep track of duplicates
 
     for line in f:
         arr = line.split(',')
-        restaurant_and_address = arr[0] + arr[2] + arr[3]   # unique key created with restaurant name and address
-        if restaurant_and_address in seen:
+        if arr[0] == "":                                        # remove restaurants with no restaurant name
             continue
         else:
-            seen.add(restaurant_and_address)                # check if the restaurant with the specific address is already in the set
-            t.write(line)
+            restaurant_and_address = arr[0] + arr[2] + arr[3]   # unique key created with restaurant name and address
+            if restaurant_and_address in seen:
+                continue
+            else:
+                seen.add(restaurant_and_address)                # check if the restaurant with the specific address is already in the set
+                t.write(line)
     return t
