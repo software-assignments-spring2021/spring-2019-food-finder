85bd7fb92b72bfde68f35ed52a120c3d9449d770
Max <mb6376@nyu.edu>
2019-05-06 20:40:46 -0400

    added bio and photo

--- src/Layouts/Team.js
+++ src/Layouts/Team.js
@@ -7,18 +7,22 @@ import CardClass from "./CardClass";
 import andrew from "./Andrew.jpg";
 import ashwin from "./Ashwin.jpg";
 import vinnie from "./Vinnie.jpg";
+import tika from "./Tika.jpg";
+import max from "./max.jpg";
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    padding: 50
+    padding: 50,
+    marginLeft: "3em",
+    marginRight: "3em"
   },
   card: {
     marginBottom: 50
   }
 });
 
-class Team extends React.Component {
+class About extends React.Component {
   render() {
     const { classes } = this.props;
     return (
@@ -30,7 +34,7 @@ class Team extends React.Component {
         <Grid container spacing={40}>
           <Grid item md className={classes.card}>
             <CardClass
-              name="Andrew Zaw"
+              name="Andrew"
               subheader="azaw502@gmail.com"
               image={andrew}
               subcontent="Andrew Zaw is currently studying Computer Science at New York University, and hails from Miami. When not messing around with the UI and React design of Rouxlette, he enjoys cooking up dishes (follow him on Instagram @chef_zaw !!) and attempting to get to Diamond on League of Legends."
@@ -48,22 +52,22 @@ class Team extends React.Component {
           <Grid item md className={classes.card}>
             <CardClass
               name="Max"
-              subheader="subheader"
-              image=""
-              subcontent="subcontent"
+              subheader="mb6376@nyu.edu"
+              image={max}
+              subcontent="Max is currently a junior at New York University studying Computer Science and minoring in Web Development. He likes comic books, videogames, and going to small museums. Follow him @spliced_tea!"
             />
           </Grid>
           <Grid item md className={classes.card}>
             <CardClass
               name="Tika"
-              subheader="subheader"
-              image=""
-              subcontent="subcontent"
+              subheader="ak5352@nyu.edu"
+              image={tika}
+              subcontent="Tika is currently a senior at NYU, majoring in Computer Science and minoring in Web Development and Game Design. In her spare time, she likes to watch anime and play horror JRPGs."
             />
           </Grid>
           <Grid item md className={classes.card}>
             <CardClass
-              name="Vinnie Zhang"
+              name="Vinnie"
               subheader="vz377@nyu.edu"
               image={vinnie}
               subcontent="Vinnie is currently a junior studying Computer Science at NYU, and is double minoring in Web Development
@@ -75,8 +79,8 @@ class Team extends React.Component {
     );
   }
 }
-Team.propTypes = {
+About.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Team);
+export default withStyles(styles)(About);
